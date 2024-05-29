8d313d85b3d71499f15f4ea07b3d936d4773d859
davirrocha <davirochamoraes7@gmail.com>
2024-05-28 22:14:10 -0300

    V0.1

--- scripts.js
+++ scripts.js
@@ -1,16 +1,23 @@
-let aparecerText = document.querySelector(".list-projects")
-let title = document.querySelector("#title")
-let text = document.querySelector("#description")
+let projectItems = document.querySelectorAll(".list-projects");
 
-aparecerText.addEventListener("mouseover", function () {
-    title.style.display = "block"
-    text.style.display = "block"
+projectItems.forEach(function (item) {
+    item.addEventListener("mouseover", function () {
+        let title = item.querySelector("#title");
+        let text = item.querySelector("#description");
+        let img = item.querySelector(".img-git")
+        title.style.display = "block";
+        text.style.display = "block";
+        img.style.display = "block"
+    });
 
-})
-
-aparecerText.addEventListener("mouseout", function () {
-    title.style.display = "none"
-    text.style.display = "none"
+    item.addEventListener("mouseout", function () {
+        let title = item.querySelector("#title");
+        let text = item.querySelector("#description");
+        let img = item.querySelector(".img-git")
+        title.style.display = "none";
+        text.style.display = "none";
+        img.style.display = "none"
+    });
 })
 
 
